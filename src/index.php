66d1c6558fcf61928f06ef6d51deaeb22e44d32a
memorynoise <tech@memorynoise.com>
2026-07-19 18:41:53 +0200

    Support for better mobile rendering

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
 <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta http-equiv="Content-Type"
           content="<?php $b('html_type') ?>; charset=<?php $b('charset') ?>"/>
     <title><?= wp_get_document_title() ?></title>
